5b25bae39196dbe40b8128dbb48361d988092a5a
Vlad Reshetov <reshetovitsme@gmail.com>
2021-09-05 20:21:25 +0300

    Edit functionality for card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Paper, PaperProps } from '@material-ui/core';
+import { Button, Paper, PaperProps, TextField } from '@material-ui/core';
 
 export interface AnCardComponentProps extends PaperProps {
     number: number;
     title: string;
     description: string;
+    editable?: boolean;
 }
 
 const AnCard = ({
     number,
     title,
     description,
+    editable,
     ...restProps
 }: AnCardComponentProps) => {
+    const [isEditable, setIsEditable] = useState<boolean | undefined>(editable);
+    const onDescriptionDoubleClickHandler = () => setIsEditable(!isEditable);
+    const onCancelClickHandler = () => setIsEditable(false);
+
     return (
         <StyledPaper {...restProps}>
             <Container>
@@ -21,7 +27,30 @@ const AnCard = ({
                     <Number>{number}</Number>
                     <Title>{title}</Title>
                 </HeaderContainer>
-                <Description>{description}</Description>
+                {isEditable ? (
+                    <Description
+                        onDoubleClick={onDescriptionDoubleClickHandler}
+                    >
+                        {description}
+                    </Description>
+                ) : (
+                    <TextAreaContainer>
+                        <TextArea
+                            id="edit-description-field"
+                            multiline
+                            defaultValue={description}
+                            variant="outlined"
+                        />
+                        <ButtonsContainer>
+                            <Button onClick={onCancelClickHandler}>
+                                Cancel
+                            </Button>
+                            <Button variant="contained" color="primary">
+                                Save
+                            </Button>
+                        </ButtonsContainer>
+                    </TextAreaContainer>
+                )}
             </Container>
         </StyledPaper>
     );
@@ -69,6 +98,25 @@ const Description = styled.p`
     font-size: 18px;
     line-height: 23px;
     text-align: justify;
+`;
+
+const TextAreaContainer = styled.div`
+    position: relative;
+`;
+
+const ButtonsContainer = styled.div`
+    position: absolute;
+    margin: 0 8px 8px 0;
+    right: 0;
+    bottom: 0;
+`;
+
+const TextArea = styled(TextField)`
+    height: 100%;
+    width: 100%;
+    .MuiOutlinedInput-multiline {
+        padding: 18.5px 14px 50px 14px;
+    }
 `;
 
 export default AnCard;
