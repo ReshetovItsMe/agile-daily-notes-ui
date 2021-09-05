a30cd9ef41489bcbb73a6f97e7e3c6cec7b58ca2
Vladislav Reshetov <reshetovitsme@gmail.com>
2021-09-05 22:14:42 +0300

    Can edit card
    * Edit functionality for card
    
    * Styles + onSave
    
    * Delete button in card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,27 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Paper, PaperProps } from '@material-ui/core';
+import {
+    Button,
+    IconButton,
+    Paper,
+    PaperProps,
+    TextField
+} from '@material-ui/core';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
 export interface AnCardComponentProps extends PaperProps {
+    id: string;
     number: number;
     title: string;
     description: string;
+    onDeleteClick: (id: string) => void;
+    onSaveClick: (value: string) => void;
+    editable?: boolean;
 }
 
 const AnCard = ({
+    id,
     number,
     title,
     description,
+    editable,
+    onSaveClick,
+    onDeleteClick,
     ...restProps
 }: AnCardComponentProps) => {
+    const [isEditable, setIsEditable] = useState<boolean | undefined>(editable);
+    const [text, setText] = useState<string>(description);
+    const handleDescriptionChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setText(event.target.value);
+    };
+    const onDescriptionDoubleClickHandler = () => setIsEditable(!isEditable);
+    const onCancelClickHandler = () => setIsEditable(false);
+    const onSaveClickHandler = () => onSaveClick(text);
+    const onDeleteClickHandler = () => onDeleteClick(id);
+
     return (
         <StyledPaper {...restProps}>
             <Container>
                 <HeaderContainer>
                     <Number>{number}</Number>
                     <Title>{title}</Title>
+                    <ButtonsContainer>
+                        {isEditable ? (
+                            <>
+                                <StyledButton onClick={onCancelClickHandler}>
+                                    Cancel
+                                </StyledButton>
+                                <StyledButton
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={onSaveClickHandler}
+                                >
+                                    Save
+                                </StyledButton>
+                            </>
+                        ) : (
+                            <IconButton
+                                aria-label="delete"
+                                onClick={onDeleteClickHandler}
+                            >
+                                <DeleteOutlinedIcon />
+                            </IconButton>
+                        )}
+                    </ButtonsContainer>
                 </HeaderContainer>
-                <Description>{description}</Description>
+                {isEditable ? (
+                    <TextArea
+                        id="edit-description-field"
+                        multiline
+                        value={text}
+                        onChange={handleDescriptionChange}
+                        variant="outlined"
+                    />
+                ) : (
+                    <Description
+                        onDoubleClick={onDescriptionDoubleClickHandler}
+                    >
+                        {description}
+                    </Description>
+                )}
             </Container>
         </StyledPaper>
     );
@@ -39,6 +103,7 @@ const Container = styled.div`
 const HeaderContainer = styled.div`
     align-items: center;
     display: flex;
+    position: relative;
 `;
 
 const Number = styled.span`
@@ -68,7 +133,42 @@ const Description = styled.p`
     font-family: PT Sans;
     font-size: 18px;
     line-height: 23px;
+    margin: 10px 0 12px 0;
     text-align: justify;
+`;
+
+const ButtonsContainer = styled.div`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+`;
+
+const StyledButton = styled(Button)`
+    .MuiButton-label {
+        font-family: 'Roboto', sans-serif;
+        font-size: 14px;
+    }
+    &.MuiButton-containedPrimary {
+        background-color: #3e3afa;
+    }
+`;
+
+const TextArea = styled(TextField)`
+    height: 100%;
+    width: 100%;
+    &.MuiFormControl-root {
+        margin-top: 10px;
+    }
+    .MuiOutlinedInput-multiline {
+        background: #f6f5fd;
+        padding: 6px 10px 6px 10px;
+    }
+    .MuiInputBase-input {
+        color: #081132;
+        font-family: PT Sans;
+        font-size: 18px;
+        line-height: 23px;
+    }
 `;
 
 export default AnCard;
